37af02c639dce0e421fe4cf98842fd8a8cec8f83
Oleksandr Hordiichuk <aleksandrhordiichuk@gmail.com>
2023-09-16 12:48:13 +0200

    test

--- Redux-practice/src/App.jsx
+++ Redux-practice/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
 
 	return (
 		<>
-
+			<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis facere aspernatur omnis quisquam recusandae illo exercitationem expedita consectetur sit obcaecati?</p>
 		</>
 	)
 }
