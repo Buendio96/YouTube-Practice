e9c094c983b5ac6d23d3236d6f067594719d8719
Oleksandr Hordiichuk <aleksandrhordiichuk@gmail.com>
2023-09-29 15:45:20 +0200

    Added several new plugins, devServer, optmization and two modules for XML/CSV files

--- WebPack/src/js/index.js
+++ WebPack/src/js/index.js
@@ -1,7 +1,12 @@
-import '../styles/style.css'
-import Page from './page.js';
-import WebpackLogo from '../assets/8BrPx64NFvs.jpg';
+import '../styles/style'
+import Page from '@models/page';
+
+import WebpackLogo from '@assets/8BrPx64NFvs';
+import xml from '@assets/data.xml'
+import csv from '@assets/example.csv'
 
 const newPost = new Page('Today I am learning Webpack technology', WebpackLogo)
 
 newPost.init()
+console.log("XML: ", xml)
+console.log("XML: ", csv)
--- WebPack/webpack.config.js
+++ WebPack/webpack.config.js
@@ -1,34 +1,65 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
 module.exports = {
 	context: path.resolve(__dirname, 'src'),
 	mode: 'development',
 	entry: {
 		index: './js/index.js',
-		page: './js/page.js',
-		some: './js/some.js'
+		page: './models/page.js',
 	},
 	output: {
 		filename: '[name].[contenthash].js',
 		path: path.resolve(__dirname, 'dist'),
 	},
+	resolve: {
+		extensions: [
+			'.js', '.json', '.css', '.jpg'
+		],
+		alias: {
+			'@models': path.resolve(__dirname, 'src/models'),
+			'@assets': path.resolve(__dirname, 'src/assets'),
+			'@': path.resolve(__dirname, 'src')
+		},
+	},
+	optimization: {
+		splitChunks: {
+			chunks: 'all'
+		}
+	},
 	plugins: [
 		new HTMLWebpackPlugin({
 			template: './index.html',
 		}),
-		new CleanWebpackPlugin()
+		new CleanWebpackPlugin(),
+		new CopyWebpackPlugin({
+			patterns: [{
+				from: path.resolve(__dirname, 'src/favicon.ico'),
+				to: path.resolve(__dirname, 'dist')
+			}]
+		})
 	],
+	devServer: {
+		port: 4200,
+	},
 	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
-			{
-				test: /\.(png|jpg|svg|gif)$/,
-				type: 'asset/resource'
-			}
-		]
+		rules: [{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader'],
+		}, {
+			test: /\.(png|jpg|svg|gif)$/,
+			type: 'asset/resource'
+		}, {
+			test: /\.(ttf|woff|sowff2|eot)$/,
+			type: 'asset/resource'
+		}, {
+			test: /\.xml$/,
+			use: 'xml-loader',
+		}, {
+			test: /\.csv$/,
+			use: 'csv-loader',
+		}]
 	}
 };
