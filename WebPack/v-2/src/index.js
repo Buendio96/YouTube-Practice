5b36b9582c3e6aa43f649b69924c16ec9423b325
Oleksandr Hordiichuk <aleksandrhordiichuk@gmail.com>
2023-10-18 15:19:45 +0200

    Added new plugins and modules for styles, fonts and babel

--- WebPack/v-2/src/index.js
+++ WebPack/v-2/src/index.js
@@ -1,27 +1,3 @@
 import './index.html'
-import './index.css'
-
-const inputEl = document.getElementById('nameInput');
-const outputEl = document.getElementById('nameWelcome');
-
-const welcome = userName => `Welcome ${userName}, I hope you feel good and still go to your goal`;
-
-let timerId;
-const delay = (f, t) => clearTimeout(timerId) || (timerId = setTimeout(f, t));
-
-const printText = (text, el, i = 0) => {
-	el.textContent = text.slice(0, i);
-	if (i < text.length) {
-		const randomDelay = Math.floor(Math.random() * (300 - 20 + 1) + 20)
-		delay(() => printText(text, el, i + 1), randomDelay)
-	}
-};
-
-inputEl.addEventListener('input', (e) => {
-	e.preventDefault();
-	delay(() => {
-		const value = inputEl.value.trim();
-		const formattedValue = value.charAt(0).toUpperCase() + value.slice(1)
-		value === '' ? outputEl.textContent = '' : printText(welcome(formattedValue), outputEl);
-	}, 1500)
-});
+import './index.scss'
+import './modules/welcome.js'
--- WebPack/v-2/webpack.config.js
+++ WebPack/v-2/webpack.config.js
@@ -9,10 +9,16 @@ const devMode = mode === 'development'
 const target = devMode ? 'web' : 'browserslist';
 const devtool = devMode ? 'source-map' : undefined;
 
+
 module.exports = {
 	mode,
 	target,
 	devtool,
+	devServer: {
+		port: 3000,
+		open: true,
+		hot: true,
+	},
 	entry: path.resolve(__dirname, 'src', 'index.js'),
 	output: {
 		path: path.resolve(__dirname, 'dist'),
@@ -32,11 +38,39 @@ module.exports = {
 			test: /\.html$/i,
 			loader: 'html-loader'
 		}, {
-			test: /\.css$/i,
+			test: /\.(c|sa|sc)ss$/i,
 			use: [
-				devMode ? "style-loader" : MiniCssExtractPlugin.loader,
-				"css-loader"
+				devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
+				'css-loader', {
+					loader: 'postcss-loader',
+					options: {
+						postcssOptions: {
+							plugins: [require('postcss-preset-env')]
+						}
+					}
+				}, 'sass-loader'
 			],
+		}, {
+			test: /\.(?:js|mjs|cjs)$/i,
+			exclude: /node_modules/,
+			use: {
+				loader: 'babel-loader',
+				options: {
+					presets: [[
+						'@babel/preset-env',
+						{ targets: "defaults" }
+					]],
+					plugins: [
+						'@babel/plugin-proposal-class-properties',
+					]
+				}
+			}
+		}, {
+			test: /\.(woff|woff2|ttf)$/i,
+			type: 'asset/resource',
+			generator: {
+				filename: 'fonts/[name].[ext]'
+			}
 		}]
 	}
 }
